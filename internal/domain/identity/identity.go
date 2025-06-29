dd7d05565b09cbe5c45ba70661ddf9e9ffe38eaf
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-06-29 14:49:54 +0200

    move creation logic to domain entity

--- internal/domain/identity/identity.go
+++ internal/domain/identity/identity.go
@@ -1,5 +1,13 @@
 package identity
 
+import (
+	"barricade/internal/infrastructure/htp"
+	"barricade/pkg/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"time"
+)
+
 type Id string
 
 type Identity struct {
@@ -8,4 +16,25 @@ type Identity struct {
 	SecretHash []byte
 	CreatedAt  int64
 	UpdatedAt  int64
+}
+
+func New(name string, secret string) (*Identity, error) {
+	if name == "" || secret == "" {
+		return nil, htp.NewError("name and secret cannot be null or empty", http.StatusBadRequest)
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(secret), 14)
+	if err != nil {
+		return nil, htp.NewError("internal error while creating secret hash", http.StatusInternalServerError)
+	}
+
+	createdAt := time.Now().UnixMilli()
+
+	return &Identity{
+		Id:         Id("ID_" + uuid.TrimmedUUID(16)),
+		Name:       name,
+		SecretHash: hash,
+		CreatedAt:  createdAt,
+		UpdatedAt:  createdAt,
+	}, nil
 }
